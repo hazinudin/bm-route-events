1f4ee4579d1c87356e063aa4e292c48ab2bc4976
hazinudin <hazinudin@hotmail.com>
2025-04-24 21:55:43 +0700

    Add RouteRoughnessValidation

---
+++ src/service/segments/validation/roughness.py
@@ -0,0 +1,269 @@
+from .base import RouteSegmentEventsValidation
+from ..analysis import segments_join
+from ..summary import Kemantapan
+from route_events import (
+    RouteRoughness, 
+    LRSRoute, 
+    RouteRoughnessRepo,
+    RouteRNI,
+    RouteRNIRepo
+)
+from ...validation_result.result import ValidationResult
+from typing import Type, Literal
+from sqlalchemy import Engine
+import polars as pl
+from pydantic import ValidationError
+
+
+class RouteRoughnessValidation(RouteSegmentEventsValidation):
+    """
+    Route Roughness Events validation class.
+    """
+    @classmethod
+    def validate_excel(
+        cls,
+        excel_path: str,
+        route: str,
+        survey_year: int,
+        survey_semester: Literal[1,2],
+        sql_engine: Engine,
+        lrs: LRSRoute, 
+        linkid_col: str = 'LINKID',
+        ignore_review: bool = False
+    ):
+        """
+        Validate RNI data in excel file.
+        """
+        result = ValidationResult(route)
+        obj = None
+        try:
+            events = RouteRoughness.from_excel(
+                excel_path=excel_path,
+                linkid=route,
+                linkid_col=linkid_col,
+                ignore_review=ignore_review,
+                data_year=survey_year,
+                data_semester=survey_semester
+            )
+
+            obj = cls(
+                route=route,
+                events=events,
+                lrs=lrs,
+                sql_engine=sql_engine,
+                results=result,
+                survey_year=survey_year,
+                survey_semester=survey_semester
+            )
+
+            return obj
+
+        except ValidationError as e:
+            for error in e.errors():
+                if 'review' in error['type']:
+                    result.add_message(error['msg'], 'review', 'review')
+                else:
+                    result.add_message(error['msg'], 'rejected')
+                
+            if result.status == 'rejected':
+                obj = cls(
+                    route=route,
+                    events=pl.DataFrame(),
+                    lrs=None,
+                    sql_engine=None,
+                    results=result,
+                    survey_year=survey_year,
+                    survey_semester=survey_semester
+                )
+
+                return obj
+            else:
+                events = RouteRoughness.from_excel(
+                    excel_path=excel_path,
+                    linkid=route,
+                    linkid_col=linkid_col,
+                    ignore_review=True,
+                    data_year=survey_year,
+                    data_semester=survey_semester
+                )
+                
+                obj = cls(
+                    route=route,
+                    events=events,
+                    lrs=lrs,
+                    sql_engine=sql_engine,
+                    results=result,
+                    survey_year=survey_year,
+                    survey_semester=survey_semester
+                )
+
+                return obj
+    def __init__(
+            self,
+            route: str,
+            events: RouteRoughness,
+            lrs: LRSRoute,
+            sql_engine: Engine,
+            results: ValidationResult,
+            survey_year: int = None,
+            survey_semester: Literal[1,2] = None
+    ):
+        super().__init__(
+            events,
+            lrs,
+            sql_engine,
+            results,
+            survey_year=survey_year,
+            survey_semester=survey_semester,
+            route=route
+        )
+
+        self._events = events
+        self._prev_data = None
+        self._prev_rni = None
+        self._rni = None
+
+        self._repo = RouteRoughnessRepo(self._engine)
+        self._rni_repo = RouteRNIRepo(self._engine)
+
+    @property
+    def prev_data(self) -> RouteRoughness:
+        """
+        Previous semester data
+        """
+        if self._prev_data is None:
+            if self._survey_sem == 2:
+                prev_year = self._survey_year
+                prev_sem = 1
+            else:
+                prev_year = self._survey_year-1
+                prev_sem = 2
+
+            self._prev_data = self._repo.get_by_linkid(
+                self._route,
+                year=prev_year,
+                semester=prev_sem,
+                raise_if_table_does_not_exists=True
+            )
+
+            self._prev_data.sta_unit='km'
+
+            return self._prev_data
+        else:
+            return self._prev_data
+        
+    @property
+    def prev_rni(self) -> RouteRNI:
+        """
+        Previous year RNI
+        """
+        if self._prev_rni is None:
+            self._prev_rni = self._rni_repo.get_by_linkid(
+                self._route, 
+                year=self._survey_year-1,
+                raise_if_table_does_not_exists=True
+            )
+
+            return self._prev_rni
+
+        else:
+            return self._prev_rni
+        
+    @property
+    def rni(self) -> RouteRNI:
+        """
+        Current year RNI
+        """
+        if self._rni is None:
+            self._rni = self._rni_repo.get_by_linkid(
+                self._route, 
+                year=self._survey_year,
+                raise_if_table_does_not_exists=True
+            )
+
+            return self._rni
+
+        else:
+            return self._rni
+
+    def kemantapan_comparison_check(self, grade_changes=2):
+        """
+        Check for segments which has kemantapan degradation compared to previous year data.
+        """
+        k = Kemantapan(
+            self._events,
+            self.rni
+        ).segment('iri_kemantapan', eager=False)
+
+        k_prev = Kemantapan(
+            self.prev_data,
+            self.prev_rni
+        ).segment('iri_kemantapan', eager=False)
+
+        errors = k.join(
+            k_prev,
+            on=[
+                self._events._linkid_col,
+                self._events._from_sta_col,
+                self._events._to_sta_col
+            ],
+            suffix='_prev'
+        ).filter(
+            # The delta between current and previous is greather than required.
+            pl.col('grade').sub(
+                pl.col('grade_prev')
+            ).ge(grade_changes)
+        ).select(
+            msg=pl.format(
+                "Segmen {}-{} mengalami penurunan kemantapan sebanyak {} tingkat jika dibandingkan dengan data tahun lalu",
+                pl.col(self._events._from_sta_col),
+                pl.col(self._events._to_sta_col),
+                pl.lit(grade_changes)
+            )
+        )
+
+        self._result.add_messages(
+            errors.collect(),
+            'review',
+            'review'
+        )
+
+        return
+
+    def rni_segments_comparison(self):
+        """
+        Check if all RNI segments match with the input events.
+        """
+        errors = segments_join(
+            self.rni,
+            self._events,
+            how='anti'
+        ).select(
+            msg = pl.format(
+                "Segmen {}-{} {} ada pada data RNI, namun tidak ada pada data ini.",
+                pl.col(self.rni._from_sta_col),
+                pl.col(self.rni._to_sta_col),
+                pl.col(self.rni._lane_code_col)
+            )
+        )
+
+        self._result.add_messages(
+            errors,
+            'error'
+        )
+
+        return
+    
+    def route_has_rni_check(self):
+        """
+        Check if route has RNI data in the same year
+        """
+        if self.rni is None:
+            msg = f"Data RNI untuk ruas {self._route} belum tervalidasi"
+            
+            self._result.add_message(
+                msg,
+                'error'
+            )
+        
+        return
