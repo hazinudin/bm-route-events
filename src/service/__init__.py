ed9689066c4648b7f7ebae0e856bb4dc6a38d4a0
hazinudin <hazinudin@hotmail.com>
2025-04-24 21:54:53 +0700

    Add RouteRoughness and RouteRoughnessRepo to the __init__

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,4 +1,8 @@
 from .bridge.master_validation import BridgeMasterValidation
 from .bridge.inventory_validation import BridgeInventoryValidation
-from .segments import RouteSegmentEventsValidation, RouteRNIValidation
+from .segments import (
+    RouteSegmentEventsValidation, 
+    RouteRNIValidation,
+    RouteRoughnessValidation
+)
 from .segments import Kemantapan
--- src/service/segments/__init__.py
+++ src/service/segments/__init__.py
@@ -1,3 +1,4 @@
 from .validation.rni import RouteRNIValidation
 from .validation.base import RouteSegmentEventsValidation
+from .validation.roughness import RouteRoughnessValidation
 from .summary import Kemantapan
