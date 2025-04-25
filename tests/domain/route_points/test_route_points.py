2cf1759a2149f963faf3f111f743dd940bd657d4
hazinudin <hazinudin@hotmail.com>
2025-04-25 16:32:28 +0700

    Add test for route point events

---
+++ tests/domain/route_points/test_route_points.py
@@ -0,0 +1,26 @@
+import unittest
+from src.route_events.points.base import RoutePointEvents
+import polars as pl
+
+
+class TestRoutePoints(unittest.TestCase):
+    def test_route_points(self):
+        df = pl.DataFrame({
+            "LINKID": ["a" for _ in range(3)],
+            "STA": [0, 10, 20],
+            "LANE_CODE": ["L1" for _ in range(3)]
+        })
+
+        e = RoutePointEvents(
+            df.to_arrow(),
+            route="a"
+        )
+
+        self.assertTrue(e.min_sta == 0)
+        self.assertTrue(e.max_sta == 20)
+        self.assertFalse(e.no_data)
+
+
+class TestRouteDefect(unittest.TestCase):
+    def test_from_excel(self):
+        self.assertTrue(True)
