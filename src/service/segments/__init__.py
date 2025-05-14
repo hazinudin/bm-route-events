3c65578cd4f0b3a49dd861e6759ec46e30518f96
hazinudin <hazinudin@hotmail.com>
2025-05-14 11:36:23 +0700

    Import module

--- src/service/segments/__init__.py
+++ src/service/segments/__init__.py
@@ -1,4 +1,5 @@
 from .validation.rni import RouteRNIValidation
 from .validation.base import RouteSegmentEventsValidation
 from .validation.roughness import RouteRoughnessValidation
+from .validation.pci import RoutePCIValidation
 from .summary import Kemantapan
