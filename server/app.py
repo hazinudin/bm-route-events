e1c9137f37e7ca4f477490442ad158e73d35bdef
hazinudin <hazinudin@hotmail.com>
2025-05-15 13:36:48 +0700

    Call put photos from defects validation end point

--- server/app.py
+++ server/app.py
@@ -228,6 +228,7 @@ class DataValidation:
 
         if write and (check.get_status() == 'verified'):
             check.put_data()
+            check.put_photos()
         
         return check.smd_output_msg(
             show_all_msg=payload.input_json.show_all_msg,
