0db1f84ac37ffec494300d7a7cfb4ba82a688b33
hazinudin <hazinudin@hotmail.com>
2025-07-30 11:50:30 +0700

    Does not explode the L4 lists if the L4 is None, not even an empty list

--- src/route_events/bridge/inventory/structure/element/element.py
+++ src/route_events/bridge/inventory/structure/element/element.py
@@ -9,22 +9,25 @@ class StructureElement(object):
         """
         Create Element object from INVIJ JSON format.
         """
-        df = pl.DataFrame(data).explode(l4_col).rename({l4_col: "_L4_STRUCT"})
-        
-        ## Old format ##
-        # df = df.with_columns(**{l4_col: pl.col("_L4_STRUCT").\
-        #                         map_elements(
-        #                             lambda s: pl.DataFrame(s).transpose(include_header=True, header_name='L4', column_names=['BHN']).drop_nulls().to_struct() if s is not None else s
-        #                             ).explode()})
+        df = pl.DataFrame(data)
 
-        df = df.with_columns(pl.col('_L4_STRUCT').struct.rename_fields([
-            "L4", "MATERIAL"
-        ]).struct.unnest())
-        
-        # df = df.with_columns(pl.col(l4_col).struct.unnest())  # Will update the BHN and L4
-        
-        df = df.select(pl.exclude('_L4_STRUCT'))  # Remove the _L4_STRUCT
-        df = df.unique(subset=["L3", "L4"])  # Will remove any duplicate, especially when L4 is null.
+        if not df[l4_col].is_null().all():  # Means the L4 contains Elements (not null)
+            df = df.explode(l4_col).rename({l4_col: "_L4_STRUCT"})
+            
+            ## Old format ##
+            # df = df.with_columns(**{l4_col: pl.col("_L4_STRUCT").\
+            #                         map_elements(
+            #                             lambda s: pl.DataFrame(s).transpose(include_header=True, header_name='L4', column_names=['BHN']).drop_nulls().to_struct() if s is not None else s
+            #                             ).explode()})
+
+            df = df.with_columns(pl.col('_L4_STRUCT').struct.rename_fields([
+                "L4", "MATERIAL"
+            ]).struct.unnest())
+            
+            # df = df.with_columns(pl.col(l4_col).struct.unnest())  # Will update the BHN and L4
+            
+            df = df.select(pl.exclude('_L4_STRUCT'))  # Remove the _L4_STRUCT
+            df = df.unique(subset=["L3", "L4"])  # Will remove any duplicate, especially when L4 is null.
 
         # Add parent attribute to Arrow Table
         df = df.with_columns(**{k.upper():pl.lit(v) for k,v in parent_keys.items()})
