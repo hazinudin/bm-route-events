9169f072606d3d82279c661e5e75d3f6d6ab2972
hazinudin <hazinudin@hotmail.com>
2025-08-04 11:06:03 +0700

    Change message ignore_in for surface_width_check to force

--- src/service/segments/validation/rni.py
+++ src/service/segments/validation/rni.py
@@ -313,7 +313,8 @@ class RouteRNIValidation(RouteSegmentEventsValidation):
 
         self._result.add_messages(
             errors,
-            'error'
+            'error',
+            'force'
         )
 
         return self
