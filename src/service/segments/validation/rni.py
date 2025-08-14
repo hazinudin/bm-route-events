04ef7c71ebb4232df5ed2418bf4d0596bf6b107c
hazinudin <hazinudin@hotmail.com>
2025-08-14 16:01:08 +0700

    Change error status for inner_shoulder_check to error_sanggah

--- src/service/segments/validation/rni.py
+++ src/service/segments/validation/rni.py
@@ -284,7 +284,8 @@ class RouteRNIValidation(RouteSegmentEventsValidation):
 
         self._result.add_messages(
             errors,
-            'error'
+            'error',
+            'force'
         )
 
         return self
