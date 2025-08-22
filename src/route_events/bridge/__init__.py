16047b4eaa74f55c4c8128d9e186467b4e4dfd8e
hazinudin <hazinudin@hotmail.com>
2025-08-22 14:21:51 +0700

    Import inventory module

--- src/route_events/bridge/__init__.py
+++ src/route_events/bridge/__init__.py
@@ -1,3 +1,4 @@
 from .master.model import BridgeMaster
 from .master.repo.repo import BridgeMasterRepo
 from .master.repo.db_repo import BridgeMasterRepoDB
+from .inventory import BridgeInventoryRepo, BridgeInventory
