e3453a6d6164e359697d9d4cc7910c8bb69f97ee
hazinudin <hazinudin@hotmail.com>
2025-08-29 17:38:26 +0700

    Remove the ListenJobResult function

--- validation-gateway/internal/job/queue.go
+++ validation-gateway/internal/job/queue.go
@@ -1,15 +1,10 @@
 package job
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"log"
 	"validation-gateway/pkg/job"
 
-	"github.com/apache/arrow/go/v16/arrow/ipc"
-	"github.com/apache/arrow/go/v16/arrow/memory"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -78,52 +73,4 @@ func (jq *JobQueue) PublishJob(job *job.ValidationJob) error {
 	}
 
 	return nil
-}
-
-func (jq *JobQueue) ListenJobResult() {
-	messages, err := jq.rabbitmqCh.Consume(
-		"result_queue",
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-
-	if err != nil {
-		log.Printf("Failed to register consumer: %v", err)
-	}
-
-	log.Print("Listening to result_queue queue.")
-
-	for msg := range messages {
-		var result map[string]string
-
-		if err := json.Unmarshal(msg.Body, &result); err != nil {
-			log.Printf("Failed to unmarshal result: %v", err)
-			continue
-		}
-
-		// Apache Arrow decoding and serialization
-		arrowBytes, err := base64.StdEncoding.DecodeString(result["result"])
-
-		if err != nil {
-			log.Printf("Failed to decode Arrow data: %v", err)
-		}
-
-		mem := memory.NewGoAllocator()
-		r := bytes.NewReader(arrowBytes)
-		reader, err := ipc.NewReader(r, ipc.WithAllocator(mem))
-
-		if err != nil {
-			log.Printf("%s", err)
-		}
-
-		for reader.Next() {
-			rec := reader.Record()
-			log.Printf("Received result from job: %s, Arrow rows: %d, Arrow schema: %s", result["jobid"], rec.NumRows(), rec.Schema())
-		}
-
-	}
 }
