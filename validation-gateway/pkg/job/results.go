75bed67a536fb0156af373204e9dac60028e6403
hazinudin <hazinudin@hotmail.com>
2025-09-01 18:03:12 +0700

    Add ValidationJobResultMessage struct

--- validation-gateway/pkg/job/results.go
+++ validation-gateway/pkg/job/results.go
@@ -1,8 +1,15 @@
 package job
 
-type ValidationJobResults struct {
+type ValidationJobResult struct {
+	JobID            string   `json:"job_id"`
 	Status           string   `json:"status"`
 	MessageCount     int      `json:"msg_count"`
 	AllMessageStatus []string `json:"all_msg_status"`
 	Ignorables       []string `json:"ignorables"`
+}
+
+type ValidationJobResultMessage struct {
+	Message       string `json:"msg" db:"msg"`
+	MessageStatus string `json:"msg_status" db:"msg_status"`
+	ContentID     string `json:"id" db:"content_id"`
 }
