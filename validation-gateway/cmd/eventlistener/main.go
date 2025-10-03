90fd01f6885dd36cebea66ecdda373a60e6b1fcc
hazinudin <hazinudin@hotmail.com>
2025-10-03 14:53:00 +0700

    Initiate trace provider

--- validation-gateway/cmd/eventlistener/main.go
+++ validation-gateway/cmd/eventlistener/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"validation-gateway/infra"
+	tracer "validation-gateway/infra/tracing"
 	"validation-gateway/internal"
 	"validation-gateway/internal/job"
 )
@@ -16,6 +20,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
+
+	// Setup context for application shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// 1. Initialize OpenTelemetry with OTLP Exporter
+	tp, err := tracer.NewTracerProvider("event-listener", ctx, conf)
+	if err != nil {
+		log.Fatalf("Failed to initialize OpenTelemetry: %v", err)
+	}
+
+	// 2. Ensure resources are properly shut down (flushing all pending traces)
+	defer func() {
+		log.Println("Shutting down OpenTelemetry Tracer Provider...")
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatalf("Error shutting down tracer provider: %v", err)
+		}
+	}()
 
 	// RabbitMQ URL
 	rmq_url := fmt.Sprintf("amqp://%s:%s", conf.RMQHost, conf.RMQPort)
