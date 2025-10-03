f22f7d95b08d07dcfd6c4bc8b2d0b3812c25691c
hazinudin <hazinudin@hotmail.com>
2025-10-03 14:49:22 +0700

    Add AmqpHeadersCarrier object for context propagation

--- validation-gateway/internal/job/queue.go
+++ validation-gateway/internal/job/queue.go
@@ -1,16 +1,44 @@
 package job
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"validation-gateway/pkg/job"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.opentelemetry.io/otel"
 )
 
 type JobQueue struct {
 	rabbitmqConn *amqp.Connection
 	rabbitmqCh   *amqp.Channel
+}
+
+type AmqpHeadersCarrier map[string]interface{}
+
+func (a AmqpHeadersCarrier) Get(key string) string {
+	v, ok := a[key]
+	if !ok {
+		return ""
+	}
+	return v.(string)
+}
+
+func (a AmqpHeadersCarrier) Set(key string, value string) {
+	a[key] = value
+}
+
+func (a AmqpHeadersCarrier) Keys() []string {
+	i := 0
+	r := make([]string, len(a))
+
+	for k := range a {
+		r[i] = k
+		i++
+	}
+
+	return r
 }
 
 func NewJobQueueClient(url string) *JobQueue {
