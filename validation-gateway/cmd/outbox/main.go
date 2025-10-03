aefcf113b17aab4977c9b5a020783bb5637e8331
hazinudin <hazinudin@hotmail.com>
2025-10-03 14:52:47 +0700

    Initiatet trace provider

--- validation-gateway/cmd/outbox/main.go
+++ validation-gateway/cmd/outbox/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	tracer "validation-gateway/infra/tracing"
 	"validation-gateway/internal"
 	"validation-gateway/internal/outbox"
 )
@@ -13,6 +17,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
+
+	// Setup context for application shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// 1. Initialize OpenTelemetry with OTLP Exporter
+	tp, err := tracer.NewTracerProvider("outbox-connector", ctx, conf)
+	if err != nil {
+		log.Fatalf("Failed to initialize OpenTelemetry: %v", err)
+	}
+
+	// 2. Ensure resources are properly shut down (flushing all pending traces)
+	defer func() {
+		log.Println("Shutting down OpenTelemetry Tracer Provider...")
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatalf("Error shutting down tracer provider: %v", err)
+		}
+	}()
 
 	connector.StartReplication()
 }
