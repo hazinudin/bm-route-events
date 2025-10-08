188fc79f0896ad459329ff6e491f72a060a2a551
hazinudin <hazinudin@hotmail.com>
2025-10-08 18:13:50 +0700

    Import BridgeEvents in the init

--- src/route_events/bridge/__init__.py
+++ src/route_events/bridge/__init__.py
@@ -1,4 +1,5 @@
 from .master.model import BridgeMaster
 from .master.repo.repo import BridgeMasterRepo
 from .master.repo.db_repo import BridgeMasterRepoDB
+from .master.events import MASTER_LENGTH_UPDATED, MASTER_NUMBER_UPDATED
 from .inventory import BridgeInventoryRepo, BridgeInventory
