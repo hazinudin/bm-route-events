a6abe6f8edbfe66b4f1569b8299b0a4630694926
hazinudin <hazinudin@hotmail.com>
2025-10-09 13:53:25 +0700

    Add BridgeMasterCoordinateUpdated event

--- src/route_events/bridge/master/events.py
+++ src/route_events/bridge/master/events.py
@@ -1,9 +1,37 @@
 from ..events import BridgeEvents
 import json
+from ...geometry import Point
 
 
 MASTER_LENGTH_UPDATED = 'master.length_updated'
 MASTER_NUMBER_UPDATED = 'master.number_updated'
+MASTER_COORDINATE_UPDATED = 'master.coordinate_updated'
+
+
+class BridgeMasterCoordinateUpdated(BridgeEvents):
+    def __init__(self, bridge_id: str, old_geom: Point, new_geom: Point):
+        BridgeEvents.__init__(self, MASTER_COORDINATE_UPDATED, bridge_id)
+        self._old_geom = old_geom
+        self._new_geom = new_geom
+
+        # Distance between new and old coordinate.
+        self._dist = old_geom.distance_to(new_geom)
+
+    def serialize(self) -> str:
+        """
+        Serialize to JSON
+        """
+        event = {
+            "name": self.name,
+            "occurred_at": self.occurred_at,
+            "event": {
+                "coordinate.old": [self._old_geom.X, self._old_geom.Y],
+                "coordinate.new": [self._new_geom.X, self._new_geom.Y],
+                "coordinate.distance": self._dist
+            }
+        }
+
+        return json.dumps(event)
 
 
 class BridgeMasterLengthUpdated(BridgeEvents):
