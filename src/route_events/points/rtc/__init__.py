af1889df27e8a08eeb3d153965928fb7e6d304d4
Hannan Azinuddin <hazinuddin@gmail.com>
2025-10-15 15:34:34 +0700

    Initial commit

---
+++ src/route_events/points/rtc/__init__.py
@@ -0,0 +1,14 @@
+from .model import (
+    RouteRTC,
+    VEH1_COL, 
+    VEH2_COL, 
+    VEH3_COL, 
+    VEH5B_COL, 
+    VEH4_COL, 
+    VEH5A_COL,
+    VEH6A_COL, 
+    VEH7A_COL,
+    VEH7B_COL,
+    VEH7C_COL, 
+    VEH8_COL,
+)
---
+++ src/route_events/points/rtc/model.py
@@ -0,0 +1,97 @@
+from ..base import RoutePointEvents
+from ...schema import RouteEventsSchema
+from pydantic import TypeAdapter
+import polars as pl
+import os
+from typing import List
+
+
+# Default column names
+VEH1_COL: str = "NUM_VEH1"
+VEH2_COL: str = "NUM_VEH2"
+VEH3_COL: str = "NUM_VEH3"
+VEH4_COL: str = "NUM_VEH4"
+VEH5A_COL: str = "NUM_VEH5A"
+VEH5B_COL: str = "NUM_VEH5B"
+VEH6A_COL: str = "NUM_VEH6A"
+VEH7A_COL: str = "NUM_VEH7A"
+VEH7B_COL: str = "NUM_VEH7B"
+VEH7C_COL: str = "NUM_VEH7C"
+VEH8_COL: str = "NUM_VEH8"
+
+
+class RouteRTC(RoutePointEvents):
+    """
+    Model of route RTC (traffic counting)
+    """
+    @classmethod
+    def from_excel(
+        cls,
+        excel_path: str,
+        linkid: str | list = 'ALL',
+        linkid_col: str = 'LINKID',
+        ignore_review: bool = False,
+        data_year: int = None,
+    ):
+        """
+        Parse data from Excel file to Arrow format and load it into RouteRTC object.
+        """
+        if type(linkid) is not str:
+            raise TypeError(f"Only accept single route in str type, not {linkid}")
+        
+        if linkid == 'ALL':
+            route_filter = pl.col(linkid_col).is_not_null()
+        else:
+            route_filter = pl.col(linkid_col).eq(linkid)
+        
+        config_path = os.path.dirname(__file__) + '/schema.json'
+
+        schema = RouteEventsSchema(
+            file_path=config_path,
+            ignore_review_err=ignore_review
+        )
+
+        df_str = pl.read_excel(
+            excel_path,
+            engine='calamine',
+            infer_schema_length=None
+        ).rename(
+            str.upper
+        ).cast(
+            pl.String
+        )
+
+        ta = TypeAdapter(List[schema.model])
+        df = pl.DataFrame(
+            ta.validate_python(df_str.to_dicts()),
+            infer_schema_length=None
+        ).filter(
+            route_filter
+        )
+
+        return cls(
+            artable=df.to_arrow(),
+            route=linkid,
+            data_year=data_year,
+            lane_data=True
+        )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Default columns
+
+        # Traffic data is not lane based data
+        self.lane_data = False
+
+    def invalid_interval(self, interval:int=15):
+        """
+        Find rows with invalid survey interval, the default survey interval is 15min
+        """
+        return
+    
+    def invalid_timestamp(self):
+        """
+        Find rows with invalid timestamp.
+        """
+        return
