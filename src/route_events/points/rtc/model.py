dd892fb5f225f99928debf3e02fe92c2006b7e17
Hannan Azinuddin <hazinuddin@gmail.com>
2025-10-17 14:23:30 +0700

    Add df_with_timestamp property

--- src/route_events/points/rtc/model.py
+++ src/route_events/points/rtc/model.py
@@ -79,11 +79,32 @@ class RouteRTC(RoutePointEvents):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Default columns
-
         # Traffic data is not lane based data
         self.lane_data = False
+        self._timestamp_col = '_timestamp'
 
+        # Default columns
+        self._hour_col: str =''
+        self._min_col: str=''
+
+    @property
+    def df_with_timestamp(self) -> pl.DataFrame:
+        """
+        Return the input data as Polars DataFrame with calculated survey timestamp (Polars Datetime)
+        """
+        return self.pl_df.with_columns(
+            **{
+                self._timestamp_col: pl.datetime(
+                    year=pl.col(self._surv_date_col).dt.year(),
+                    month=pl.col(self._surv_date_col).dt.month(),
+                    day=pl.col(self._surv_date_col).dt.day(),
+                    hour=pl.col(self._hour_col),
+                    minute=pl.col(self._min_col),
+                    second=pl.lit(0),
+                )
+            }
+        )
+        
     def invalid_interval(self, interval:int=15):
         """
         Find rows with invalid survey interval, the default survey interval is 15min
@@ -93,5 +114,11 @@ class RouteRTC(RoutePointEvents):
     def invalid_timestamp(self):
         """
         Find rows with invalid timestamp.
+        """
+        return
+    
+    def survey_duration(self) -> int:
+        """
+        Get the duration of the surveys in minutes.
         """
         return
