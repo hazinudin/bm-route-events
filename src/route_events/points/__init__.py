f144276066c75ce660a9d6da7ac09be919fbea73
Hannan Azinuddin <hazinuddin@gmail.com>
2025-10-22 17:57:15 +0700

    Include RouteRTC and RouteRTCRepo

--- src/route_events/points/__init__.py
+++ src/route_events/points/__init__.py
@@ -1,3 +1,5 @@
 from .base.base import RoutePointEvents
 from .defect.model import RouteDefects
 from .defect.repo import RouteDefectsRepo
+from .rtc.model import RouteRTC
+from .rtc.repo import RouteRTCRepo
