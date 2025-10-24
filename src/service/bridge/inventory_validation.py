91c97b1bd8fff5ea621b7b8f17394f0aeb000ee7
Hannan Azinuddin <hazinuddin@gmail.com>
2025-10-24 14:33:09 +0700

    Continue checks even though the results is error

--- src/service/bridge/inventory_validation.py
+++ src/service/bridge/inventory_validation.py
@@ -165,10 +165,7 @@ class BridgeInventoryValidation(object):
             self.has_subs_check()
             self.master_data_distance_check()
             self.lrs_distance_check()
-    
-        if self.get_status() == 'error':
-            return
-        
+
         self.compare_total_span_length_to_inv_length_check()
         self.main_span_structure_type_check()
         self.main_span_num_check()
