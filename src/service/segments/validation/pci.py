d61b6295496e416eecbfc2c42a86df75a4343fc4
Hannan Azinuddin <hazinuddin@gmail.com>
2025-12-03 14:48:50 +0700

    Exclude last segment for rni_surf_type_segment_length_check

--- src/service/segments/validation/pci.py
+++ src/service/segments/validation/pci.py
@@ -368,7 +368,8 @@ class RoutePCIValidation(RouteSegmentEventsValidation):
                 pl.col(self._events._seg_len_col).lt(0.05 - tolerance) |
                 pl.col(self._events._seg_len_col).gt(0.05 + tolerance)
             ).and_(
-                pl.col(self._events._from_sta_col + '_r').ne(self._events.max_from_sta*self._events.sta_conversion)
+                pl.col(self._events._from_sta_col + '_r').cast(pl.Int32).ne(self._events.max_from_sta*self._events.sta_conversion) &
+                pl.col(self._events._to_sta_col + '_r').cast(pl.Int32).ne(self._events.max_to_sta*self._events.sta_conversion)
             )
         ).select(
             msg = pl.when(
