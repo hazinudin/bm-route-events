467ce85301c2f7d3247077cb12253935a7c888f8
Hannan Azinuddin <hazinudin@hotmail.com>
2026-04-10 14:27:53 +0700

    feat: Add semester support and lane_data to RouteFWDRepo

--- src/route_events/points/fwd/repo.py
+++ src/route_events/points/fwd/repo.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 class RouteFWDRepo(object):
     def __init__(self, sql_engine: Engine):
-        self._table = 'fwd'
+        self._table = "fwd"
         self._engine = sql_engine
         self._inspect = inspect(sql_engine)
 
@@ -17,11 +17,22 @@ class RouteFWDRepo(object):
     def table(self):
         return self._table
 
+    def _full_table_name(
+        self,
+        year: int,
+        semester: int,
+    ):
+        """
+        The FWD table full name.
+        """
+        return f"{self.table}_{semester}_{year}"
+
     def get_by_linkid(
-            self,
-            linkid: str,
-            year: int,
-            raise_if_table_does_not_exists: bool = False
+        self,
+        linkid: str,
+        year: int,
+        semester: int = 2,
+        raise_if_table_does_not_exists: bool = False,
     ) -> RouteFWD:
         """
         Get Route FWD data from database and load it into RouteFWD object.
@@ -29,33 +40,37 @@ class RouteFWDRepo(object):
         if type(year) is not int:
             raise TypeError("'year' is not integer.")
 
-        query = f"select * from {self.table}_{year} where linkid = '{linkid}'"
+        full_table_name = self._full_table_name(year, semester)
 
-        if not self._inspect.has_table(f"{self.table}_{year}") and raise_if_table_does_not_exists:
-            raise NoSuchTableError(f"Table {self.table}_{year} does not exists")
+        query = f"select * from {full_table_name} where linkid = '{linkid}'"
 
-        df = pl.read_database(
-            query,
-            connection=self._engine,
-            infer_schema_length=None
-        )
+        if (
+            not self._inspect.has_table(f"{full_table_name}")
+            and raise_if_table_does_not_exists
+        ):
+            raise NoSuchTableError(f"Table {full_table_name} does not exists")
 
-        obj = RouteFWD(
-            df.to_arrow(),
-            route=linkid,
-            data_year=year
-        )
+        df = pl.read_database(query, connection=self._engine, infer_schema_length=None)
+
+        obj = RouteFWD(df.to_arrow(), route=linkid, data_year=year, lane_data=True)
 
         return obj
 
-    def put(self, events: RouteFWD, year: int):
+    def put(self, events: RouteFWD, year: int, semester: int = 2):
         """
         Put Route FWD data into FWD geodatabase table.
         """
-        with self._engine.connect() as conn, conn.execution_options(isolation_level='READ COMMITTED'):
+        with (
+            self._engine.connect() as conn,
+            conn.execution_options(isolation_level="READ COMMITTED"),
+        ):
             try:
-                self._delete(events, conn=conn, commit=False, year=year)
-                self._insert(events, conn=conn, commit=False, year=year)
+                self._delete(
+                    events, conn=conn, commit=False, year=year, semester=semester
+                )
+                self._insert(
+                    events, conn=conn, commit=False, year=year, semester=semester
+                )
             except Exception as e:
                 conn.rollback()
                 raise e
@@ -64,14 +79,17 @@ class RouteFWDRepo(object):
 
         return
 
-    def _delete(self, events: RouteFWD, year: int, conn, commit: bool = True):
+    def _delete(
+        self, events: RouteFWD, year: int, semester: int, conn, commit: bool = True
+    ):
         """
         Delete FWD data from FWD geodatabase table.
         """
         _where = f" where {events._linkid_col} = '{events.route_id}'"
-        _del_stt = f"delete from {self.table}_{year}" + _where
+        full_table_name = self._full_table_name(year, semester)
+        _del_stt = f"delete from {full_table_name}" + _where
 
-        if self._inspect.has_table(f"{self.table}_{year}"):
+        if self._inspect.has_table(full_table_name):
             try:
                 conn.execute(text(_del_stt))
             except Exception as e:
@@ -81,55 +99,52 @@ class RouteFWDRepo(object):
             if commit:
                 conn.commit()
 
-    def _insert(self, events: RouteFWD, year: int, conn, commit: bool = True):
+    def _insert(
+        self, events: RouteFWD, year: int, semester: int, conn, commit: bool = True
+    ):
         """
         Insert FWD data into FWD geodatabase table.
         """
+        full_table_name = self._full_table_name(year, semester)
         try:
-            if self._inspect.has_table(f"{self.table}_{year}"):
-                if has_objectid(f"{self._table}_{year}", self._engine):
+            if self._inspect.has_table(full_table_name):
+                if has_objectid(full_table_name, self._engine):
                     oids = generate_objectid(
-                        schema='smd',
-                        table=f"{self.table}_{year}",
+                        schema="smd",
+                        table=full_table_name,
                         sql_engine=self._engine,
-                        oid_count=events.pl_df.select(pl.len()).rows()[0][0]
+                        oid_count=events.pl_df.select(pl.len()).rows()[0][0],
                     )
 
-                    args = [pl.Series('OBJECTID', oids)]
+                    args = [pl.Series("OBJECTID", oids)]
 
                 else:
                     args = []
 
                 events.pl_df.with_columns(
-                    pl.lit(datetime.now()).dt.datetime().alias('UPDATE_DATE'),
-                    pl.lit(0).alias('COPIED'),
-                    *args
+                    pl.lit(datetime.now()).dt.datetime().alias("UPDATE_DATE"),
+                    pl.lit(0).alias("COPIED"),
+                    *args,
                 ).write_database(
-                    f"{self.table}_{year}",
+                    full_table_name,
                     connection=conn,
-                    if_table_exists='append',
+                    if_table_exists="append",
                     engine_options={
-                        'dtype': ora_pl_dtype(
-                            events.pl_df,
-                            date_cols_keywords='DATE'
-                        )
-                    }
+                        "dtype": ora_pl_dtype(events.pl_df, date_cols_keywords="DATE")
+                    },
                 )
 
             else:
                 events.pl_df.with_columns(
-                    pl.lit(datetime.now()).dt.datetime().alias('UPDATE_DATE'),
-                    pl.lit(0).alias('COPIED')
+                    pl.lit(datetime.now()).dt.datetime().alias("UPDATE_DATE"),
+                    pl.lit(0).alias("COPIED"),
                 ).write_database(
-                    f"{self.table}_{year}",
+                    full_table_name,
                     connection=conn,
-                    if_table_exists='append',
+                    if_table_exists="append",
                     engine_options={
-                        'dtype': ora_pl_dtype(
-                            events.pl_df,
-                            date_cols_keywords='DATE'
-                        )
-                    }
+                        "dtype": ora_pl_dtype(events.pl_df, date_cols_keywords="DATE")
+                    },
                 )
 
         except Exception as e:
