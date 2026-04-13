6a6be2a27699f36d06b616c7cbfff793ed4ed10c
Hannan Azinuddin <hazinudin@hotmail.com>
2026-04-13 15:02:40 +0700

    defects: use bm-photo SurveyPhotoStorage, photo_id from URL_PHOTO, fix base_validation bug

--- src/service/points/validation/defects.py
+++ src/service/points/validation/defects.py
@@ -1,11 +1,6 @@
 from .base import RoutePointEventsValidation
-from ...photo import gs
-from route_events import (
-    RouteDefects,
-    RouteDefectsRepo,
-    LRSRoute,
-    SurveyPhoto
-)
+from ...photo.client import SurveyPhotoStorage
+from route_events import RouteDefects, RouteDefectsRepo, LRSRoute, SurveyPhoto
 from ..analysis import segments_points_join
 from sqlalchemy import Engine
 from typing import List
@@ -18,6 +13,7 @@ class RouteDefectsValidation(RoutePointEventsValidation):
     """
     Route Defects Events validation class.
     """
+
     @classmethod
     def validate_excel(
         cls,
@@ -26,8 +22,8 @@ class RouteDefectsValidation(RoutePointEventsValidation):
         survey_year: int,
         sql_engine: Engine,
         lrs: LRSRoute,
-        photo_storage: gs.SurveyPhotoStorage,
-        linkid_col: str = 'LINKID',
+        photo_storage: SurveyPhotoStorage,
+        linkid_col: str = "LINKID",
         ignore_review: bool = False,
         force_write: bool = False,
     ):
@@ -37,10 +33,10 @@ class RouteDefectsValidation(RoutePointEventsValidation):
         ignored_tag = []
 
         if force_write:
-            ignored_tag.append('force')
-        
+            ignored_tag.append("force")
+
         if ignore_review:
-            ignored_tag.append('review')
+            ignored_tag.append("review")
 
         result = ValidationResult(route, ignore_in=ignored_tag)
 
@@ -52,7 +48,7 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                 linkid=route,
                 linkid_col=linkid_col,
                 ignore_review=ignore_review,
-                data_year=survey_year
+                data_year=survey_year,
             )
 
             obj = cls(
@@ -62,19 +58,19 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                 sql_engine=sql_engine,
                 results=result,
                 survey_year=survey_year,
-                photo_storage=photo_storage
+                photo_storage=photo_storage,
             )
 
             return obj
-        
+
         except ValidationError as e:
             for error in e.errors():
-                if 'review' in error['type']:
-                    result.add_message(error['msg'], 'review', 'review')
+                if "review" in error["type"]:
+                    result.add_message(error["msg"], "review", "review")
                 else:
-                    result.add_message(error['msg'], 'rejected')
-        
-            if result.status == 'rejected':
+                    result.add_message(error["msg"], "rejected")
+
+            if result.status == "rejected":
                 obj = cls(
                     route=route,
                     events=pl.DataFrame(),
@@ -82,18 +78,18 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                     sql_engine=sql_engine,
                     results=result,
                     survey_year=survey_year,
-                    photo_storage=photo_storage
+                    photo_storage=photo_storage,
                 )
 
                 return obj
-            
+
             else:
                 events = RouteDefects.from_excel(
                     excel_path=excel_path,
                     linkid=route,
                     linkid_col=linkid_col,
                     ignore_review=True,
-                    data_year=survey_year
+                    data_year=survey_year,
                 )
 
                 obj = cls(
@@ -103,13 +99,15 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                     sql_engine=sql_engine,
                     results=result,
                     survey_year=survey_year,
-                    photo_storage=photo_storage
+                    photo_storage=photo_storage,
                 )
 
                 return obj
-        
+
         except IndexError:
-            result.add_message(f"File '{excel_path}' tidak dapat ditemukan.", 'rejected')
+            result.add_message(
+                f"File '{excel_path}' tidak dapat ditemukan.", "rejected"
+            )
 
             obj = cls(
                 route=route,
@@ -118,20 +116,20 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                 sql_engine=sql_engine,
                 results=result,
                 survey_year=survey_year,
-                photo_storage=photo_storage
+                photo_storage=photo_storage,
             )
 
             return obj
 
     def __init__(
-            self,
-            route: str,
-            events: RouteDefects,
-            lrs: LRSRoute,
-            sql_engine: Engine,
-            results: ValidationResult,
-            photo_storage: gs.SurveyPhotoStorage,
-            survey_year: int = None
+        self,
+        route: str,
+        events: RouteDefects,
+        lrs: LRSRoute,
+        sql_engine: Engine,
+        results: ValidationResult,
+        photo_storage: SurveyPhotoStorage,
+        survey_year: int = None,
     ):
         super().__init__(
             events=events,
@@ -139,7 +137,7 @@ class RouteDefectsValidation(RoutePointEventsValidation):
             sql_engine=sql_engine,
             results=results,
             route=route,
-            survey_year=survey_year
+            survey_year=survey_year,
         )
 
         self._repo = RouteDefectsRepo(self._engine)
@@ -150,7 +148,7 @@ class RouteDefectsValidation(RoutePointEventsValidation):
 
         # Photo storage
         self._storage = photo_storage
-    
+
     @property
     def survey_photos(self) -> List[SurveyPhoto]:
         """
@@ -158,49 +156,39 @@ class RouteDefectsValidation(RoutePointEventsValidation):
         """
         if self._photos is None:
             self._photos = [
-                SurveyPhoto(**_) for _ in self._events.pl_df.join(
+                SurveyPhoto(**_)
+                for _ in self._events.pl_df.join(
                     self.df_lrs_mv,
                     on=[
                         self._events._linkid_col,
                         self._events._sta_col,
-                        self._events._lane_code_col
-                    ]
-                ).select(
-                    pl.when(
-                        pl.col(self._events._photo_url_cols).str.starts_with(self._storage.root_url)
-                    ).then(
-                        pl.col(self._events._photo_url_cols)
-                    ).otherwise(
-                        pl.format(
-                            # Filename must include /<province folder>/<route>/<file name>
-                            "{}/{}/{}",
-                            pl.lit(self._storage.root_url),
-                            pl.col(self._events._year_col),
-                            pl.col(self._events._photo_url_cols)
-                        )
-                    ).alias('url'),
-                    pl.col('m_val').alias('sta_meters'),
-                    pl.lit(self._survey_year).alias('survey_year'),
-                    pl.col(self._events._linkid_col).alias('linkid'),
-                    pl.col(self._events._lat_col).alias('latitude'),
-                    pl.col(self._events._long_col).alias('longitude')
-                ).rows(named=True) 
+                        self._events._lane_code_col,
+                    ],
+                )
+                .select(
+                    pl.col(self._events._photo_url_cols).alias("photo_id"),
+                    pl.col("m_val").alias("sta_meters"),
+                    pl.lit(self._survey_year).alias("survey_year"),
+                    pl.col(self._events._linkid_col).alias("linkid"),
+                    pl.col(self._events._lat_col).alias("latitude"),
+                    pl.col(self._events._long_col).alias("longitude"),
+                )
+                .rows(named=True)
             ]
             return self._photos
         else:
             return self._photos
-        
+
     def survey_photo_url_check(self):
         """
-        Validate photo URL or filename which is stored in the repository.
+        Validate photo ID which is stored in the repository.
         """
-        for invalid in self._storage.validate_photos_url(self.survey_photos, return_invalid=True):
-            msg = f"{invalid.url} bukan merupakan URL valid atau gambar tidak ditemukan."
+        for invalid in self._storage.validate_photos_url(
+            self.survey_photos, return_invalid=True
+        ):
+            msg = f"{invalid.photo_id} bukan merupakan photo ID valid atau gambar tidak ditemukan."
 
-            self._result.add_message(
-                msg,
-                'error'
-            )
+            self._result.add_message(msg, "error")
 
         return
 
@@ -209,53 +197,57 @@ class RouteDefectsValidation(RoutePointEventsValidation):
         Compare defects surface type with surface type in RNI.
         """
         if self._events._surf_type_col == self.rni._surf_type_col:
-            suffix = '_r'
+            suffix = "_r"
         else:
-            suffix = ''
+            suffix = ""
 
         # All available surface type mapping from the defect and RNI
-        surf_types = pl.concat(
-            [self._events.surface_type_mapping, self.rni.surface_types_mapping]
-        ).unique('surf_type').select(['surf_type', 'category'])
-
-        errors = segments_points_join(
-            segments=self.rni,
-            points=self._events,
-            how='inner',
-            point_select=[self._events._surf_type_col],
-            segment_select=[self.rni._surf_type_col],
-            suffix='_r'
-        ).join(
-            surf_types,
-            left_on=self._events._surf_type_col,
-            right_on='surf_type',
-        ).join(
-            surf_types,
-            left_on=self.rni._surf_type_col+suffix,
-            right_on='surf_type',
-            suffix=suffix
-        ).filter(
-            pl.col('category').ne(
-                pl.col('category'+suffix)
+        surf_types = (
+            pl.concat(
+                [self._events.surface_type_mapping, self.rni.surface_types_mapping]
             )
-        ).select(
-            msg=pl.format(
-                "Tipe perkerasan pada STA {} {} tidak sama dengan data RNI, yaitu {} sedangkan data RNI {}",
-                pl.col(self._events._sta_col).truediv(self._events.sta_conversion).cast(pl.Int64),
-                pl.col(self._events._lane_code_col),
-                pl.col('category'),
-                pl.col('category'+suffix)
+            .unique("surf_type")
+            .select(["surf_type", "category"])
+        )
+
+        errors = (
+            segments_points_join(
+                segments=self.rni,
+                points=self._events,
+                how="inner",
+                point_select=[self._events._surf_type_col],
+                segment_select=[self.rni._surf_type_col],
+                suffix="_r",
+            )
+            .join(
+                surf_types,
+                left_on=self._events._surf_type_col,
+                right_on="surf_type",
+            )
+            .join(
+                surf_types,
+                left_on=self.rni._surf_type_col + suffix,
+                right_on="surf_type",
+                suffix=suffix,
+            )
+            .filter(pl.col("category").ne(pl.col("category" + suffix)))
+            .select(
+                msg=pl.format(
+                    "Tipe perkerasan pada STA {} {} tidak sama dengan data RNI, yaitu {} sedangkan data RNI {}",
+                    pl.col(self._events._sta_col)
+                    .truediv(self._events.sta_conversion)
+                    .cast(pl.Int64),
+                    pl.col(self._events._lane_code_col),
+                    pl.col("category"),
+                    pl.col("category" + suffix),
+                )
             )
         )
 
-        self._result.add_messages(
-            errors,
-            'review',
-            'review'
-        )
+        self._result.add_messages(errors, "review", "review")
 
         return
-    
+
     def damage_surface_type_check(self):
         """
         This method will compare the damage type with the surface type in RNI.
@@ -267,40 +259,38 @@ class RouteDefectsValidation(RoutePointEventsValidation):
 
         df_surf_types = pl.DataFrame(surface_types)
 
-        errors = self._events.pl_df.join(
-            df_surf_types,
-            left_on=[self._events._surf_type_col],
-            right_on=['surf_type'],
-            how='inner'
-        ).filter(
-            pl.col(self._events._defects_type_col).str.starts_with('AS').and_(
-                pl.col('category').eq('paved')
-            ).and_(
-                pl.col(self._events._surf_type_col).eq(21)
-            ) |
-            pl.col(self._events._defects_type_col).str.starts_with('RG').and_(
-                pl.col('category').eq('paved')
-            ).and_(
-                pl.col(self._events._surf_type_col).ne(21)
-            ) |
-            pl.col('category').eq('unpaved')
-        ).select(
-            msg=pl.format(
-                "Tipe kerusakan {} pada STA {} {} tidak sesuai dengan tipe perkerasan",
-                pl.col(self._events._defects_type_col),
-                pl.col(self._events._sta_col),
-                pl.col(self._events._lane_code_col)
+        errors = (
+            self._events.pl_df.join(
+                df_surf_types,
+                left_on=[self._events._surf_type_col],
+                right_on=["surf_type"],
+                how="inner",
+            )
+            .filter(
+                pl.col(self._events._defects_type_col)
+                .str.starts_with("AS")
+                .and_(pl.col("category").eq("paved"))
+                .and_(pl.col(self._events._surf_type_col).eq(21))
+                | pl.col(self._events._defects_type_col)
+                .str.starts_with("RG")
+                .and_(pl.col("category").eq("paved"))
+                .and_(pl.col(self._events._surf_type_col).ne(21))
+                | pl.col("category").eq("unpaved")
+            )
+            .select(
+                msg=pl.format(
+                    "Tipe kerusakan {} pada STA {} {} tidak sesuai dengan tipe perkerasan",
+                    pl.col(self._events._defects_type_col),
+                    pl.col(self._events._sta_col),
+                    pl.col(self._events._lane_code_col),
+                )
             )
         )
 
-        self._result.add_messages(
-            errors,
-            'review',
-            'review'
-        )
+        self._result.add_messages(errors, "review", "review")
 
         return
-    
+
     def damage_severity_check(self):
         """
         Check severity for damages registered.
@@ -310,31 +300,19 @@ class RouteDefectsValidation(RoutePointEventsValidation):
                 "Titik {} {} memiliki kerusakan {} tanpa nilai severity.",
                 pl.col(self._events._sta_col),
                 pl.col(self._events._lane_code_col),
-                pl.col(self._events._defects_type_col)
+                pl.col(self._events._defects_type_col),
             )
         )
 
-        self._result.add_messages(
-            error,
-            "error"
-        )
+        self._result.add_messages(error, "error")
 
         return
-    
+
     def put_data(self):
         """
         Delete and insert events data to geodatabase table.
         """
         self._repo.put(self._events, year=self._survey_year)
-
-    def put_photos(self):
-        """
-        Put photo to photo database
-        """
-        self._storage.register_photos(
-            photos=self.survey_photos,
-            validate=False
-        )
 
     def base_validation(self):
         """
@@ -345,7 +323,7 @@ class RouteDefectsValidation(RoutePointEventsValidation):
         self.lrs_sta_check()
         self.route_has_rni_check()
         self.sta_not_in_rni_check()
-        self.survey_photo_url_check
+        self.survey_photo_url_check()
 
         self.surface_type_check()
         self.damage_surface_type_check()
