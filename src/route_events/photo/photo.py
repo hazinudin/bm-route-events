980a15286e6b9659e434191402947d3d64f484d7
Hannan Azinuddin <hazinudin@hotmail.com>
2026-04-13 15:02:30 +0700

    model: add photo_id field to SurveyPhoto dataclass

--- src/route_events/photo/photo.py
+++ src/route_events/photo/photo.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass
 from urllib.parse import unquote
 
+
 @dataclass
 class SurveyPhoto(object):
     """
     Survey photo with its attribute
     """
-    url: str
-    sta_meters: float
-    survey_year: int
-    linkid: str
-    latitude: float
-    longitude: float
+
+    photo_id: str
+    url: str = ""
+    sta_meters: float = 0.0
+    survey_year: int = 0
+    linkid: str = ""
+    latitude: float = 0.0
+    longitude: float = 0.0
 
     def unquoted_url(self):
         """
