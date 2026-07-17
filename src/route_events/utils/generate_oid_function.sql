d4b82971b7782c12e86d06b41c9c79ac70db4af5
Hannan Azinuddin <hazinudin@hotmail.com>
2026-07-17 17:18:33 +0700

    fix(generate_oid): remove varray to fix ORA-06532 for batch sizes >32767

--- src/route_events/utils/generate_oid_function.sql
+++ src/route_events/utils/generate_oid_function.sql
@@ -1,21 +1,12 @@
-CREATE OR REPLACE FUNCTION generate_oid(
+create or replace FUNCTION generate_oid(
     oid_count in number,
-    table_name in varchar,
+    table_name  in varchar,
     schema_name in varchar
 )
-    return sys.odcinumberlist pipelined as
-    my_array sys.odcinumberlist;
+    return sys.odcinumberlist pipelined
+as
 BEGIN
-    my_array := sys.odcinumberlist();
-    my_array.extend(oid_count);
-    
-    -- Your PL/SQL code that populates the array
-    for i in 1..oid_count loop
-        my_array(i) := sde.gdb_util.next_rowid(schema_name, table_name);
-    end loop;
-    
-    -- Convert array to table for selection
     FOR i IN 1..oid_count LOOP
-        pipe row(my_array(i));
+        pipe row( sde.gdb_util.next_rowid(schema_name, table_name) );
     END LOOP;
 END;
